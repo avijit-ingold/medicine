397eb45b6bd6e239506bb517ad59d98fcd8bcf14
AzureAD\AnishRajak <anish.rajak@ingoldsolutions.com>
2025-03-20 00:23:50 +0530

    Front-end Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, useNavigate, navigate } from "react-router-dom";
 import AppRoutes from "./Routes";
 import GenericApiProvider from "./context/GenericApiProvider";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 
 export default function App() {
